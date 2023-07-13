d0e0a6e89a5cda87e0a19a4bc3f98b70b71599e2
Sloth247 <yukohori0118@gmail.com>
2023-07-13 15:20:38 -0700

    added backend files

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import jobs from '../data';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useSearchParams, Link } from 'react-router-dom';
+import { listJobs } from '../actions/jobActions';
+// import jobs from '../data';
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
+  const jobList = useSelector((state) => state.jobList);
+  const { loading, error, jobs, page, pages } = jobList;
+
+  useEffect(() => {
+    dispatch(listJobs());
+  }, [dispatch]);
+
   return (
-    <div>
-      {jobs.map((job) => (
-        <p>{job.company}</p>
-      ))}
-    </div>
+    <div>{Array.isArray(jobs) && jobs.map((job) => <p>{job.company}</p>)}</div>
   );
 };
 
